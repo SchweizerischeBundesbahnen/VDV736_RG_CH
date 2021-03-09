ced925dcb4b83fa561a698790c3b9df53f2f3046
u215619 <u215619@sbb.ch>
2021-03-09 08:55:11 +0100

    trying to fix build error

--- xmllint-check.sh
+++ xmllint-check.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /usr/bin/find . -name "*.xsd" -type f | while read i; do XMLLINT_INDENT=" " xmllint --pretty 1 "$i" > "$i.pretty"; mv "$i.pretty" "$i"; done; /usr/bin/find . -name "*.xml" -type f | while read i; do XMLLINT_INDENT=" " xmllint --pretty 1 "$i" > "$i.pretty"; mv "$i.pretty" "$i"; done; /usr/bin/find . -name "*.wsdl" -type f | while read i; do XMLLINT_INDENT=" " xmllint --pretty 1 "$i" > "$i.pretty"; mv "$i.pretty" "$i"; done;
 echo "finished formatting"
-find examples/ -iname "*.xml" | xargs -0 xmllint --noout --schema xsd/siri.xsd
+find examples/ -iname "*.xml" | xargs -d '\n' xmllint --noout --schema xsd/siri.xsd
